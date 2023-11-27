14845976c99c3ab8c82bbc0ab762c90067c95c60
Xakimov-Sardor <sardor.xakimov.dev@yandex.ru>
2023-11-27 17:33:45 +0500

    i destroy all

--- website/functions.py
+++
@@ -1,8 +0,0 @@
-def get_datetime():
-    from datetime import datetime
-
-    now = datetime.now()
-
-    now_format = f'{now.hour}:{now.minute}:{now.second} - {now.day}/{now.month}/{now.year}'
-
-    return now_format
--- website/views.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-views = Blueprint('view', __name__)
-
-@views.route('/')
-def home():
-
-    return render_template('home.html')
