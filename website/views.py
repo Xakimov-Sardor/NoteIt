25134a53d21c741fba61a5dbce0ba9a6a7ec2c13
Xakimov-Sardor <sardor.xakimov.dev@yandex.ru>
2023-12-04 14:04:03 +0500

    test commit for kate

--- website/views.py
+++ website/views.py
@@ -21,7 +21,7 @@ def create_wall():
     if request.method == 'POST':
         name = request.form.get('name')
         
-        if len(name) >= 10:
+        if len(name) >= 2:
 
             new_wall = Wall(name=name, user_id=current_user.id)
             db.session.add(new_wall)
@@ -53,19 +53,26 @@ def wall_view(id):
 @login_required
 def update_wall(wall_id):
         wall = Wall.query.filter_by(id=wall_id, user_id=current_user.id).first()
-
-        if wall:
-            wall.name = 'CHANGED BY SCRIPT'
-
+        if request.method == 'POST':
             
-            db.session.commit()
+            if wall:
+                new_name = request.form.get('name')
 
-            db.session.refresh(wall)
+                if len(new_name) >= 2:
+                    wall.name = new_name
 
-            return redirect(url_for('views.home'))
+                
+                    db.session.commit()
+
+                    db.session.refresh(wall)
+
+
+                    return redirect(url_for('views.wall_view', id=wall.id))
+                else:
+                    flash('Please longer', category='error')
     
 
-        return render_template('update_wall.html')
+        return render_template('update_wall.html', c_user=current_user, wall=wall)
 
 @views.route('/wall/delete/<wall_id>')
 @login_required
@@ -74,6 +81,10 @@ def delete_wall(wall_id):
     wall = Wall.query.filter_by(id=wall_id, user_id=current_user.id).first()
     
     if wall:
+        for note in wall.notes:
+            note = Note.query.filter_by(wall_id=wall_id).first()
+            db.session.delete(note)
+
         db.session.delete(wall)
         db.session.commit()
 
@@ -86,7 +97,18 @@ def delete_wall(wall_id):
 @views.route('/wall/clear/<wall_id>')
 @login_required
 def clear_wall(wall_id):
-    return redirect(url_for('views.home'))
+    wall = Wall.query.filter_by(id=wall_id, user_id=current_user.id).first()
+
+    if wall:
+        for note in wall.notes:
+            note = Note.query.filter_by(wall_id=wall_id).first()
+            db.session.delete(note)
+        
+        db.session.commit()
+
+
+
+    return redirect(url_for('views.wall_view', id=wall_id))
 
 @views.route('/note/<wall_id>/create', methods=['POST', 'GET'])
 @login_required
@@ -114,6 +136,27 @@ def create_note(wall_id):
 @views.route('/note/<wall_id>/update/<note_id>', methods=['POST', 'GET'])
 @login_required
 def update_note(wall_id, note_id):
+
+    note = Note.query.filter_by(wall_id=wall_id, id=note_id).first()
+
+    if request.method == 'POST':
+        if note:
+            title = request.form.get('title')
+            text = request.form.get('text')
+
+            if len(title) > 2 and len(text) > 5:
+                note.title = title
+                note.text = text
+
+                db.session.commit()
+
+                db.session.refresh()
+
+                return redirect(url_for('views.wall_view', id=wall_id))
+            else:
+                flash('Please longer', category='error')
+
+
     return render_template('update_note.html')
 
 @views.route('/note/<wall_id>/delete/<note_id>')
