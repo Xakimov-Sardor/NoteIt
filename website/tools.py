54049493c558af843848c7dc49a99d797621efa8
Xakimov-Sardor <sardor.xakimov.dev@yandex.ru>
2023-12-04 17:12:29 +0500

    i restart it

--- website/tools.py
+++
@@ -1,9 +0,0 @@
-def get_time():
-    from datetime import datetime
-    return datetime.now()
-
-
-def email_format(email):
-    from re import match
-    temp = '[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+'
-    return match(temp, email)
